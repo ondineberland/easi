1f5416cf05ac98ebeaab6914d4c332d3706080ef
Ondine Berland <ondineberland@MacBook-Air-de-Ondine.local>
2023-10-02 15:43:19 +0200

    Create notes.R
    Some notes, some descriptions

---
+++ R/notes.R
@@ -0,0 +1,11 @@
+## Ondine's notes ##
+
+## Step by Step
+
+### 0. Data
+#Load data 
+
+### 1. Modules
+#Run easi.R
+#Run intermediate blocs.R ## In summary, the intermediate.blocs function processes an input object to recover and calculate various coefficients and sums related to equations. These calculations are based on the "EASI made EASIER" methodology by PENDAKUR (2008). The function returns a list containing all the recovered and calculated values.##
+#
